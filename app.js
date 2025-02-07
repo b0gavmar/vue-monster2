545138996012ec2f12ad4e581ae8fd99dd543809
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-02-07 13:26:57 +0100

    monsterattack

--- app.js
+++ app.js
@@ -21,26 +21,33 @@ const app = vue.createApp({
   methods: {
     playerAttack() {
       const attack = this.getRandomNumber(5, 12);
+      this.monsterHealth = Math.max(monsterHealth-attack,0);
+      this.monsterAttack();
     },
     monsterAttack() {
       const attack = this.getRandomNumber(8, 12);
+      this.monsterHealth = Math.max(playerHealth-attack,0);
     },
-    specialAttack(){
-        const attack = this.getRandomNumber(10, 25);
+    specialAttack() {
+      const attack = this.getRandomNumber(10, 25);
+      this.monsterHealth = Math.max(monsterHealth-attack,0);
+      this.monsterAttack();
     },
     playerHeal() {
       const healValue = this.getRandomNumber(8, 20);
+      this.playerHealth = Math.min(playerHealth+healValue,100);
+      this.monsterAttack();
     },
-    surrender(){
-        this.winner ="Monster won"
+    surrender() {
+      this.winner = "Monster won";
     },
-    newGame(){
-        this.monsterHealth = 100;
-        this.playerHealth = 100;
-        this.winner = null;
-        this.logs = [];
-        this.round = 0;
-    }
+    newGame() {
+      this.monsterHealth = 100;
+      this.playerHealth = 100;
+      this.winner = null;
+      this.logs = [];
+      this.round = 0;
+    },
   },
 });
 
